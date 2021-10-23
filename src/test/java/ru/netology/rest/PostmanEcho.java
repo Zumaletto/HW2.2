b34017af76d340150b89952eb1dbb4c1bf231d97
Kirillova Olesya <olesyak-85@mail.ru>
2021-10-23 21:06:35 +0300

    all test passed

--- src/test/java/ru/netology/rest/PostmanEcho.java
+++ src/test/java/ru/netology/rest/PostmanEcho.java
@@ -16,7 +16,7 @@ public class PostmanEcho {
                 .when()
                 .post("/post")
                 .then()
-                .statusCode(201)
+                .statusCode(200)
                 .body("data", equalTo("first test"));
     }
 
@@ -30,7 +30,7 @@ public class PostmanEcho {
                 .when()
                 .post("/post")
                 .then()
-                .statusCode(201)
+                .statusCode(200)
                 .body("data", equalTo("second test"))
                 .contentType(ContentType.JSON);
     }
