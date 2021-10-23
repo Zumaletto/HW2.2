9d7cce85f31ca4fcb91757992d14e18d058dc24f
Kirillova Olesya <olesyak-85@mail.ru>
2021-10-23 20:59:29 +0300

    added test

--- src/main/java/ru/netology/rest/PostmanEcho.java
+++
@@ -1,7 +0,0 @@
-package ru.netology.rest;
-
-import jdk.jfr.StackTrace;
-
-public class PostmanEcho {
-
-}
---
+++ src/test/java/ru/netology/rest/PostmanEcho.java
@@ -0,0 +1,37 @@
+package ru.netology.rest;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEcho {
+    @Test
+    void shouldSendPostToPostmanEcho() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("first test")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("first test"));
+    }
+
+    @Test
+    void shouldSendPostToPostmanEcho2() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("second test")
+                .header("Content-Type", "application/json; charset=UTF-8")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("second test"))
+                .contentType(ContentType.JSON);
+    }
+}
